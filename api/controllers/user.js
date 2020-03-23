c938a6f753dd33bc956bf8eacaaa399e15698697
Sebastian Rodriguez Zapata <48870936+sRodriguezZ@users.noreply.github.com>
2020-03-23 14:44:26 -0300

    register_user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 
 function home (req, res){
@@ -11,7 +12,40 @@ function pruebas (req, res){
     res.status(200).send({ message: 'Pruebas servidor NodeJS' });
 }
 
+function saveUser(req, res){
+    var params = req.body;
+    var user = new User();
+    
+    if(params.name && params.surname && params.nick && params.email && params.password){
+        user.name = params.name;
+        user.surname = params.surname;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.role = 'ROLE_USER';
+        user.image = null;
+
+        //encriptar contraseña
+        bcrypt.hash(params.password, null, null, (err, hash) => {
+            user.password = hash;
+            user.save((err, userStored) => {
+                if(err){
+                    return res.status(500).send({ message: 'Error al guardar el usuario'});
+                }
+
+                if(userStored){
+                    res.status(200).send({user: userStored});
+                }else{
+                    res.status(404).send({ message: 'No se ha registrado el usuario'});
+                }
+            });
+        });
+    }else{
+        res.status(200).send({ message: 'Enviar todos los campos' });
+    }
+}
+
 module.exports = {
     home,
-    pruebas
+    pruebas,
+    saveUser
 }
--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ var port = 3800;
 
 // Conexion database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://192.168.0.26:27017/curso-mean-social', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://192.168.0.26:27017/curso_mean_social', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("La conexion a la base de datos es correcta");
         // crear servidor
