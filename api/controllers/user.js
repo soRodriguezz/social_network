f2612d36acaf0921109fcf6ae6da7c18216b008a
Sebastian Rodriguez Zapata <48870936+sRodriguezZ@users.noreply.github.com>
2020-03-23 19:13:46 -0300

    upload_image

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,8 +1,11 @@
 'use strict' 
 
 var bcrypt = require('bcrypt-nodejs');
+var mongoosePaginate = require('mongoose-pagination');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
+var fs = require('fs');
+var path = require('path');
 
 function home(req, res) {
     res.status(200).send({message: 'Hola mundo / raiz'});
@@ -115,10 +118,93 @@ function getUser(req, res){
     });
 }
 
+function getUsers(req,res){
+    var identity_user_id = req.user.sub;
+    var page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 5;
+
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) =>{
+        if(err) return res.status(500).send({ message: 'Error en la peticion'});
+
+        if(!users){
+            return res.status(404).send({ message: 'No hay usuarios disponibles'});
+        }
+
+        return res.status(200).send({ users, total, pages: Math.ceil(total/itemsPerPage)});
+    });
+}
+
+function updateUser(req, res){
+    var userId = req.params.id;
+    var update = req.body;
+
+    //borrar propiedad password
+    delete update.password;
+
+    if(userId != req.user.sub){
+        return res.status(500).send({ message: 'No tienes permiso para actualizar'});
+    }
+
+    User.findByIdAndUpdate(userId, update, {new: true}, (err, userUpdate) => {
+        if(err) return res.status(500).send({ message: 'Error en la peticion'});
+
+        if(!userUpdate){
+            return res.status(404).send({ message: 'No se ha podido actualizar usuario'});
+        }
+
+        return res.status(200).send({ user: userUpdate});
+    });
+}
+
+function uploadImage(req, res){
+    var userId = req.params.id;
+
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        
+
+        var file_name = file_split[2];
+        console.log(file_name);
+
+        var ext_split = file_name.split('\.');
+        console.log(ext_split);
+
+        var file_ext = ext_split[1];
+        
+        if(userId != req.user.sub){
+            removeFilesOfUploads(res, file_path, 'No tienes permiso para actualizar');
+        }
+
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
+
+        }else{
+            removeFilesOfUploads(res, file_path, 'extension no valida');
+        }
+
+    }else{
+        return res.status(200).send({ message: 'No se han subido imagenes'});
+    }
+}
+
+function removeFilesOfUploads(res, file_path, message){
+    fs.unlink(file_path, (err) => {
+            return res.status(200).send({ message: message});
+    });
+}
+
 module.exports = {
     home,
     pruebas,
     saveUser,
     loginUser,
-    getUser
+    getUser,
+    getUsers,
+    updateUser,
+    uploadImage
 }
