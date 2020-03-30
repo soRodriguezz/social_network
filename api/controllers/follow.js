9a3c10a03ea2252e5edda5443bbdc73ff34540db
Sebastian Rodriguez Zapata <48870936+sRodriguezZ@users.noreply.github.com>
2020-03-30 12:01:45 -0300

    follow

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -36,9 +36,41 @@ function deleteFollow(req, res){
     });
 }
 
+function getFollowingUsers(req, res){
+    var userId = req.user.sub;
+
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+
+    var page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    }else{
+        page = req.params.id;
+    }
+
+    var itemsPerPage = 4;
+
+    Follow.find({ user: userId}).populate({ path: 'followed' }).paginate(page, itemsPerPage, (err, follows, total) => {
+        if(err) return res.status(500).send({ message: 'Error en el servidor'});
+
+        if(!follows){
+            return res.status(404).send({ message: 'No hay follows'});
+        }
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        })
+    });
+}
 
 
 module.exports = {
     saveFollow,
-    deleteFollow
+    deleteFollow,
+    getFollowingUsers
 }
